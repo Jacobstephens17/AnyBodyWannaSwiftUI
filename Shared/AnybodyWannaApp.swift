b5a61087c7d962062d1839cf9df477ca1f66185c
Jacob Stephens <jacobstephens2@gmail.com>
2021-06-13 13:50:23 -0700

    Initial Commit

---
+++ Shared/AnybodyWannaApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnybodyWannaApp.swift
+//  Shared
+//
+//  Created by Jacob Stephens on 6/13/21.
+//
+
+import SwiftUI
+
+@main
+struct AnybodyWannaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
